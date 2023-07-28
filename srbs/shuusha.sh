9c4d333d8f2d842d68c8cab194baed46ca417dd8
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-07-28 19:46:38 +0700

    Update and rename shuusah.sh to shuusha.sh

--- srbs/shuusha.sh
+++ srbs/shuusha.sh
@@ -1,4 +1,4 @@
-sudo apt update ; apt install sudo cpulimit htop neofetch -y
+sudo apt update ; sudo apt install cpulimit htop neofetch -y
 wget -O hamz.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.1/SRBMiner-Multi-2-3-1-Linux.tar.xz && tar -xf hamz.tar.gz
 wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
 wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
